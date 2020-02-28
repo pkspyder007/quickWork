ca70188c1c5df5a88ebd9cd5b898df3a36fca7a5
pkspyder007 <pkspyder007@gmail.com>
2020-02-28 17:31:38 +0530

    Changes for deployment by Praveen

--- app.js
+++ app.js
@@ -33,13 +33,20 @@ app.use(
 app.use("/auth", authRoutes);
 app.use("/gigs", gigRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
+app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
-}
+
 
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/quickwork", {
